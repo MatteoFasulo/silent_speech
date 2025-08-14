c91e35c0db7bef2fd62b607b05107147412d1109
MatteoFasulo <mat.fasulo@gmail.com>
2025-08-14 17:22:06 +0200

    Update removing torchaudio resample as already included inside transcribe file call of speechbrain ASR
    - iterate over target and prediction to remove empty strings

--- asr_evaluation.py
+++ asr_evaluation.py
@@ -8,7 +8,6 @@ import jiwer
 from unidecode import unidecode
 import tqdm
 
-device = "cuda"
 asr = EncoderASR.from_hparams(
     source="speechbrain/asr-wav2vec2-librispeech",
     savedir="pretrained_models/asr-wav2vec2-librispeech",
@@ -21,20 +20,18 @@ def evaluate(testset, audio_directory):
     targets = []
 
     for i, datapoint in enumerate(tqdm.tqdm(testset, "Evaluate outputs", disable=None)):
-        audio, rate = torchaudio.load(os.path.join(audio_directory, f"example_output_{i}.wav"))
+        text = asr.transcribe_file(os.path.join(audio_directory, f"example_output_{i}.wav"))
 
-        if rate != 16000:
-            audio = torchaudio.functional.resample(audio, rate, 16000)
-
-        text = asr.transcribe_batch(wavs=audio, wav_lens=torch.tensor([1.0], device=device))
-
-        pred_text = text[0] if isinstance(text[0], str) else str(text[0])
-
-        pred_text = testset.text_transform.clean_text(pred_text)
-        target_text = testset.text_transform.clean_text(datapoint["text"])
+        pred_text = testset.text_transform.clean_text(text)
+        target_text = testset.text_transform.clean_text(datapoint['text'])
 
         predictions.append(pred_text)
         targets.append(target_text)
+
+    for i, (targ, pred) in enumerate(zip(targets, predictions)):
+        if targ == '':
+            del targets[i]
+            del predictions[i]
 
     for i in range(len(targets)):
         logging.info(f"Target: {targets[i]}")
